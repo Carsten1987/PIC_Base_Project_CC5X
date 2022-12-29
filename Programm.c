d6fc33b3025e40db7a47f9ee7c5a02609305b475
Carsten1987 <75756062+Carsten1987@users.noreply.github.com>
2022-12-29 21:30:28 +0100

    Update für CC5X Basis

---
+++ Programm.c
@@ -0,0 +1,2 @@
+// Besteht ein Programm aus mehreren C-Files, müssen beim CC5X diese über eine C-File kombiniert werden.
+#include "main.c"
---
+++ main.c
@@ -0,0 +1,17 @@
+/**
+ * @file main.c
+ * @author Carsten Keller (carsten@kellercarsten.de)
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-29
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <16F628.H>
+
+void main(void)
+{
+
+}
